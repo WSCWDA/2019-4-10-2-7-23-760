87a44fb98ddee5aec4b90ebd94e3793d0654508b
chengwendi <347895316@qq.com>
2019-05-31 10:35:48 +0800

    commit code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    if(inputs.distance < 2){
+    	return 6 + Math.round(inputs.parkTime*0.25);
+    }else if(inputs.distance <8){
+    	return 6 + Math.round((inputs.distance-2)*0.8 +inputs.parkTime*0.25);
+    }else{
+    	return 6 + Math.round(0.8*6 + (inputs.distance-8)*1.2 +inputs.parkTime*0.25);
+    }
 };
